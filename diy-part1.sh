a35c775f174792ac1797753eb88460afcb3a6d7d
wyc <r@wyc.me>
2022-08-27 16:52:25 +0800

    Add feed sources

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,14 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+# Add feed sources
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git' >>feeds.conf.default
+echo 'src-git cloudflarespeedtest https://github.com/yinchaow/luci-app-cloudflarespeedtest.git' >>feeds.conf.default
+echo 'src-git netspeedtest https://github.com/sirpdboy/NetSpeedTest.git' >>feeds.conf.default
+
+# Add luci-app-poweroff
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
