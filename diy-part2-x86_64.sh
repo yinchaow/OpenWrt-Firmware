698013c91da53672fd665021ed6c46226b0d4b43
wyc <r@wyc.me>
2022-08-28 16:26:09 +0800

    Change default IP

--- diy-part2-x86_64.sh
+++ diy-part2-x86_64.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.7.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.7.8/g' package/base-files/files/bin/config_generate
 
 # Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
